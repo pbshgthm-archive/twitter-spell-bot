280b9e8d5185aa34f21f53d7f50bafc174280083
pbshgthm <hitchhikerz@outlook.com>
2017-06-28 19:31:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 bot_auth={}
 bot_auth['consumer_key']="T33FjrY4RGpe9NEzgQ954OUSn"
 bot_auth['consumer_secret']="PhAv8jmwl6v81Dwoa6f3wwI9sVwJlOGKPvPtXddqLFJt555KJm"
-bot_auth['access_token']="879776151593406464-spfPfVk6zt8ua2BHuzSCO7gGVj7fALh"
-bot_auth['access_token_secret']="bnS4i0js9jTR0q1AwuLY9LJGWxn226C6y5AFo0kAinJKT"
+bot_auth['access_token']="879776151593406464-ElU8aommFKYgGnumhnLfYknSMvF3MjY"
+bot_auth['access_token_secret']="cOikyceR9XJ1ZwouWgrDAvBBssArRD3ldkxOxt8RaeySy"
 bot_auth['handle']="iamSpellCop"
